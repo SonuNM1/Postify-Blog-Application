cccc517ac181f8baae5a9739e9f24fe026c5584b
Sonu N Mahto <139614905+SonuNM1@users.noreply.github.com>
2025-01-10 14:15:05 +0530

    es module

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,11 @@
-// tailwind.config.js
-module.exports = {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+
+
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
